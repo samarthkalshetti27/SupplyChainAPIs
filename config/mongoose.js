d0d981411835b853656cbadbd7b0a51a62a7401d
Samarth Kalshetti <samarthkalshetti407@gmail.com>
2022-08-21 17:00:35 +0530

    20

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  `mongodb+srv://Farzeen:Farzeen8998@cluster0.cnirj.mongodb.net/BiofuelsDb?retryWrites=true&w=majority`
-);
+const {URL} = require("./envs")
+mongoose.connect(URL);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
