0e49d0bfa8f19371a70e581e4edfef0264b31963
Samarth Kalshetti <samarthkalshetti407@gmail.com>
2022-08-25 17:10:21 +0530

    sih2

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const fileStorage = require("../middleware/fileStorage");
 const farmerController = require("../controllers/farmer");
 router.post("/login", userController.login);
-router.post("/addpost", fileStorage.array("images"), farmerController.addPost);
+router.post("/addpost", farmerController.addPost);
 router.get("/getpost", farmerController.getPost);
 module.exports = router;
